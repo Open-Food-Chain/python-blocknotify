4f3b9eac9b7d3b65b3e74c28b76b64b68df61aed
MyloHDEX <mylo@hybriddex.com>
2020-09-22 08:52:03 +0000

    dockerfile & flake8

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,21 @@
 FROM python:3.8
 ENV PYTHONUNBUFFERED 1
+# from https://rtfm.co.ua/en/docker-configure-tzdata-and-timezone-during-build/
+ENV TZ=Etc/UTC
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && \
+  echo $TZ > /etc/timezone
+
+RUN apt-get update \
+  && apt-get install -y php php-gmp
+
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 RUN mkdir /code
+RUN mkdir /code/lib
+COPY run.sh /code
+COPY test.py /code
+COPY genaddressonly.php /code
+COPY lib/rpclib.py /code/lib
 WORKDIR /code
 
 COPY ./ /code/
--- test.py
+++ test.py
@@ -1,29 +1,27 @@
 from lib import rpclib
 from slickrpc import Proxy
-import binascii
 import requests
 import subprocess
 import json
-import sys
 
 
 # global vars
 rpc_user = "changeme"
 rpc_password = "alsochangeme"
-port =  24708
+port = 24708
 
-komodo_node_ip = "127.0.0.1"
+komodo_node_ip = "172.29.0.2"
 
 this_node_address = "RLw3bxciVDqY31qSZh8L4EuM2uo3GJEVEW"
 this_node_pubkey = "02f2cdd772ab57eae35996c0d39ad34fe06304c4d3981ffe71a596634fa26f8744"
 this_node_wif = "UpUiqKNj43SBPe9SvYqpygZE3BS83f87GVQSV8zXt2Gr813YZ3Ah"
 
-rpc_connect = rpc_connection = Proxy("http://%s:%s@127.0.0.1:%d"%(rpc_user, rpc_password, port));
+rpc_connect = rpc_connection = Proxy("http://%s:%s@127.0.0.1:%d" % (rpc_user, rpc_password, port))
 
 
 django_base_url = "http://172.29.0.4:8777/"
 DEV_IMPORT_API_JCF_BATCH_INTEGRITY_PATH = "integrity/"
-DEV_IMPORT_API_JCF_BATCH_REQUIRE_INTEGRITY_PATH= "raw/refresco/require_integrity/"
+DEV_IMPORT_API_JCF_BATCH_REQUIRE_INTEGRITY_PATH = "raw/refresco/require_integrity/"
 DEV_IMPORT_API_RAW_REFRESCO_REQUIRE_INTEGRITY_PATH = "raw/refresco/require_integrity/"
 DEV_IMPORT_API_RAW_REFRESCO_INTEGRITY_PATH = "raw/refresco-integrity/"
 
@@ -31,22 +29,22 @@ blocknotify_chainsync_limit = 5
 housekeeping_address = "RS7y4zjQtcNv7inZowb8M6bH3ytS1moj9A"
 
 
-#check wallet management
+# check wallet management
 is_mine = rpclib.validateaddress(rpc_connect, this_node_address)['ismine']
 
-if is_mine == False:
+if is_mine is False:
     rpclib.importprivkey(rpc_connect, this_node_wif)
 
 is_mine = rpclib.validateaddress(rpc_connect, this_node_address)['ismine']
 
-#start housekeeping
+# start housekeeping
 
 # we send this amount to an address for housekeeping
 # update by 0.0001 (manually, if can be done in CI/CD, nice-to-have not need-to-have) (MYLO)
 # house keeping address is list.json last entry during dev
 script_version = 0.00010005
 
-general_info = rpclib.getinfo(rpc_connect);
+general_info = rpclib.getinfo(rpc_connect)
 sync = general_info['longestchain'] - general_info['blocks']
 
 print(general_info['longestchain'])
@@ -93,6 +91,7 @@ print(res)
 # batch record import database id(uuid) = $3
 ###########################
 
+
 def import_jcf_batch_integrity_pre_process(wallet, data, import_id):
 
     data = json.dumps(data)
@@ -105,7 +104,7 @@ def import_jcf_batch_integrity_pre_process(wallet, data, import_id):
     print(item_address['address'])
 
     url = django_base_url + DEV_IMPORT_API_JCF_BATCH_INTEGRITY_PATH
-    data={'name': 'chris', 'integrity_address': item_address['address'], 'batch': import_id}
+    data = {'name': 'chris', 'integrity_address': item_address['address'], 'batch': import_id}
 
     res = requests.post(url, data=data)
 
@@ -120,13 +119,14 @@ def import_jcf_batch_integrity_pre_process(wallet, data, import_id):
     print(response)
 
     url = django_base_url + DEV_IMPORT_API_JCF_BATCH_INTEGRITY_PATH + id + "/"
-    data={'name': 'chris', 'integrity_address': item_address['address'], 'integrity_pre_tx': response}
+    data = {'name': 'chris', 'integrity_address': item_address['address'], 'integrity_pre_tx': response}
 
     res = requests.put(url, data=data)
 
     print(res.text)
 
     return
+
 
 def get_address(wallet, data):
     signed_data = rpclib.signmessage(rpc_connect, wallet, data)
@@ -135,6 +135,7 @@ def get_address(wallet, data):
     item_address = json.loads(item_address)['address']
 
     return item_address
+
 
 def import_raw_refresco_batch_integrity_pre_process(wallet, data, import_id):
 
@@ -156,7 +157,7 @@ def import_raw_refresco_batch_integrity_pre_process(wallet, data, import_id):
     print(item_address['address'])
 
     url = django_base_url + DEV_IMPORT_API_RAW_REFRESCO_INTEGRITY_PATH
-    data={'name': 'chris', 'integrity_address': item_address['address'], 'batch': import_id}
+    data = {'name': 'chris', 'integrity_address': item_address['address'], 'batch': import_id}
 
     res = requests.post(url, data=data)
 
@@ -171,7 +172,7 @@ def import_raw_refresco_batch_integrity_pre_process(wallet, data, import_id):
     print(response)
 
     url = django_base_url + DEV_IMPORT_API_RAW_REFRESCO_INTEGRITY_PATH + id + "/"
-    data={'name': 'chris', 'integrity_address': item_address['address'], 'integrity_pre_tx': response}
+    data = {'name': 'chris', 'integrity_address': item_address['address'], 'integrity_pre_tx': response}
 
     res = requests.put(url, data=data)
 
@@ -189,6 +190,7 @@ def import_raw_refresco_batch_integrity_pre_process(wallet, data, import_id):
 def sign_message():
     return
 
+
 print("start improt api")
 
 url = django_base_url + DEV_IMPORT_API_JCF_BATCH_REQUIRE_INTEGRITY_PATH
@@ -197,7 +199,7 @@ res = requests.get(url)
 
 raw_json = res.text
 
-#batches_null_integrity = array(batches_null_integrity)
+# batches_null_integrity = array(batches_null_integrity)
 
 batches_null_integrity = ""
 
@@ -225,7 +227,7 @@ print(res.text)
 
 raw_json = res.text
 
-#batches_null_integrity = array(batches_null_integrity)
+# batches_null_integrity = array(batches_null_integrity)
 
 batches_null_integrity = ""
 
