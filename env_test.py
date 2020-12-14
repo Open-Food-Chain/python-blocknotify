f42c6d264e865c42032e9b3fa39a066e6afc1f9c
diesmaster <chris@vanmaarseveen.net>
2020-12-14 14:04:46 +0100

    myenv

--- env_test.py
+++ env_test.py
@@ -1,13 +1,5 @@
-from lib.openfood_env import EXPLORER_URL
-#from lib.openfood_env import THIS_NODE_WALLET
-#from lib.openfood_env import THIS_NODE_WIF
-#from lib.openfood_env import TEST_GEN_WALLET_PASSPHRASE
-#from lib.openfood_env import TEST_GEN_WALLET_ADDRESS
-#from lib.openfood_env import TEST_GEN_WALLET_WIF
-#from lib.openfood_env import TEST_GEN_WALLET_PUBKEY
-#from lib.openfood_env import openfood_API_BASE_URL
-#from lib.openfood_env import openfood_API_ORGANIZATION_CERTIFICATE
-#from lib import openfood
+from lib import openfood_env 
+from lib import openfood
 
 from dotenv import load_dotenv
 import json
@@ -15,8 +7,7 @@ import json
 import os
 load_dotenv(verbose=True)
 
-chris = os.getenv('CHRIS')
-print(chris)
 
-chris = os.environ['CHRIS']
-print(chris)
+def test_explorer():
+	assert True == True
+
